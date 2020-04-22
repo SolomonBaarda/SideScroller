c04f1c7c3a80c92d32ca06424d09fc545cc28d18
SolomonBaarda <solomon_baarda@outlook.com>
2020-04-22 11:12:35 +0100

    Improved buff scriptable object

--- Assets/Scripts/Buffs/Buff.cs
+++ Assets/Scripts/Buffs/Buff.cs
@@ -2,36 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+[CreateAssetMenu]
 public class Buff : ScriptableObject
 {
-    protected BuffName buffName;
+    [SerializeField] private BuffName buffName;
 
     // Movement buffs
-    protected float speed_bonus = 0;
-    protected float jump_power_bonus = 0;
-    protected int double_jump_bonus = 0;
+    [SerializeField] [Range(0, 1)] private float speed_bonus_multiplier;
+    [SerializeField] [Range(0, 1)] private float jump_power_bonus_multiplier;
+    [SerializeField] [Range(0, 1)] private int double_jump_bonus;
 
     // Damage buffs
-    protected float damage_bonus = 0;
-    protected float projectile_speed_bonus = 0;
-    protected float splash_damage_area_bonus = 0;
+    [SerializeField] [Range(0, 1)] private float damage_bonus_multiplier;
+    [SerializeField] [Range(0, 1)] private float projectile_speed_bonus_multiplier;
+    [SerializeField] [Range(0, 1)] private float splash_damage_area_bonus_multiplier;
 
     // Health buffs
-    protected int max_health_bonus = 0;
-    protected float damage_resistance_bonus = 0;
+    [SerializeField] [Range(0, 1)] private float max_health_bonus_multiplier;
+    [SerializeField] [Range(0, 1)] private float damage_resistance_bonus_multiplier;
 
     // Inventory buffs
-    protected int weapon_inventory_size_bonus = 0;
+    [SerializeField] [Range(0, 1)] private int weapon_inventory_size_bonus = 0;
+
+
+    // Sprite to display
+    public Sprite sprite;
 
 
     public void CombineBuffs(Buff b)
     {
-        // Combine all field totals 
+        speed_bonus_multiplier += b.speed_bonus_multiplier;
+        jump_power_bonus_multiplier += b.jump_power_bonus_multiplier;
+        double_jump_bonus += b.double_jump_bonus;
+
+        damage_bonus_multiplier += b.damage_bonus_multiplier;
+        projectile_speed_bonus_multiplier += b.projectile_speed_bonus_multiplier;
+        splash_damage_area_bonus_multiplier += b.splash_damage_area_bonus_multiplier;
+
+        max_health_bonus_multiplier += b.max_health_bonus_multiplier;
+        damage_resistance_bonus_multiplier += b.damage_resistance_bonus_multiplier;
+
+        weapon_inventory_size_bonus += b.weapon_inventory_size_bonus;
     }
+
+
 
     public enum BuffName
     {
-
+        TOTAL_BUFFS_COMBINED,
+        SpeedBoost,
+        Jump_Power,
+        DoubleJump
     }
 
 }
