eb1f341d722e4731b16cb306bfb51d30ef2b8c55
SolomonBaarda <solomon_baarda@outlook.com>
2020-04-29 12:10:02 +0100

    Added slide boost

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,33 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float speed = 40f;
-    [SerializeField] private float jump_force = 600f;
+    [SerializeField] private float speed = 40;
+    [SerializeField] private float jump_force = 12;
     [SerializeField] private int default_double_jumps = 1;
     [SerializeField] private int max_double_jumps = 0;
     [SerializeField] private int double_jumps_left = 0;
     [Range(0, 1)] [SerializeField] private float crouch_speed_reduction = .4f;
 
     [Header("Controller Settings")]
-    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .02f;
+    [Range(0, .5f)] [SerializeField] private float movementSmoothing = .02f;
     [SerializeField] private bool allowAirControl = true;
     private const float collisionCheckRadius = .2f;
+
+    public readonly Vector2 TERMINAL_VELOCITY = new Vector2(16, 124);
+    private Vector2 previousVelocity = Vector2.zero;
+    private bool previouslyGrounded = false;
+
+    [SerializeField] private Direction facing;
+
     private Rigidbody2D rigid;
     private Collider2D mainCollider;
     private Collider2D feetCollider;
-
-    private Vector2 velocity = Vector2.zero;
-    [SerializeField]
-    private Direction facing;
     public Transform headPos;
     public Transform feetPos;
 
     private enum Direction { Left, Right };
+
 
     private void Awake()
     {
@@ -54,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // Check if the player is on the ground
-        bool wasGrounded = IsOnGround;
+        previouslyGrounded = IsOnGround;
         IsOnGround = false;
 
         // Get all collisions
@@ -91,7 +96,7 @@ public class PlayerMovement : MonoBehaviour
         // Allow user direction movement input 
         if (IsOnGround || allowAirControl)
         {
-            // Crouching
+            // Set crouching
             if (crouch)
             {
                 // Player has just crouched this frame
@@ -106,7 +111,7 @@ public class PlayerMovement : MonoBehaviour
                     mainCollider.enabled = false;
                 }
             }
-            // Not crouching
+            // Set not crouching
             else
             {
                 // Enable the collider when not crouching
@@ -122,34 +127,69 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
 
+            // Movement stuff
+
             // Desired velocity
             Vector2 targetVelocity = new Vector2(move * 10, rigid.velocity.y);
+
+            bool isSliding = false;
 
             // Player is crouching, overwrite target vel
             if (IsCrouching && IsOnGround)
             {
+                // Player has just landed from falling
+                if(!previouslyGrounded)
+                {
+                    // Do a big slide in the direction the player is facing
+
+                    Vector2 newVelocity = rigid.velocity;
+
+                    int dir = 0;
+                    if (facing.Equals(Direction.Right))
+                    {
+                        dir = 1;
+                    }
+                    else if (facing.Equals(Direction.Left))
+                    {
+                        dir = -1;
+                    }
+
+                    newVelocity.x = dir * Mathf.Abs(previousVelocity.y);
+                    rigid.velocity = newVelocity;
+                }
+
                 // Reduce the speed by the crouch speed reduction
                 targetVelocity.x = Mathf.MoveTowards(rigid.velocity.x, 0, crouch_speed_reduction);
+                isSliding = true;
             }
 
-            //velocity = rigid.velocity;
-            // And then smoothing it out and applying it to the character
-            rigid.velocity = Vector2.SmoothDamp(rigid.velocity, targetVelocity, ref velocity, movementSmoothing);
-            //rigid.velocity = Vector2.MoveTowards(rigid.velocity, targetVelocity, Mathf.Abs(move));
-                
 
-            // If the input is moving the player right and the player is facing left
-            if (move > 0 && !facing.Equals(Direction.Right))
+            // Get the new velocity and ensure it doesn't exceed terminal velocity
+            Vector3 newVel = Vector2.SmoothDamp(rigid.velocity, targetVelocity, ref previousVelocity, movementSmoothing);
+            newVel.x = Mathf.Clamp(newVel.x, -TERMINAL_VELOCITY.x, TERMINAL_VELOCITY.x);
+            newVel.y = Mathf.Clamp(newVel.y, -TERMINAL_VELOCITY.y, TERMINAL_VELOCITY.y);
+
+            // Assign the new velocity
+            previousVelocity = rigid.velocity = newVel;
+
+
+            // Can't change direction when sliding
+            if (!isSliding)
             {
-                // Face right
-                Face(Direction.Right);
+                // If the input is moving the player right and the player is facing left
+                if (move > 0 && !facing.Equals(Direction.Right))
+                {
+                    // Face right
+                    Face(Direction.Right);
+                }
+                // Otherwise if the input is moving the player left and the player is facing right
+                else if (move < 0 && !facing.Equals(Direction.Left))
+                {
+                    // Face left
+                    Face(Direction.Left);
+                }
             }
-            // Otherwise if the input is moving the player left and the player is facing right
-            else if (move < 0 && !facing.Equals(Direction.Left))
-            {
-                // Face left
-                Face(Direction.Left);
-            }
+
         }
 
         // Regular jump
@@ -178,7 +218,7 @@ public class PlayerMovement : MonoBehaviour
         rigid.velocity = vel;
 
         // Then jump
-        rigid.AddForce(new Vector2(0, force));
+        rigid.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
     }
 
 
@@ -188,7 +228,7 @@ public class PlayerMovement : MonoBehaviour
         rigid.velocity = Vector2.zero;
 
         // Then jump
-        rigid.AddForce(new Vector2(forceX, forceY));
+        rigid.AddForce(new Vector2(forceX, forceY), ForceMode2D.Impulse);
     }
 
 
