d6543371a517e13aaf556b3c8c2889ee7bed9151
SolomonBaarda <solomon_baarda@outlook.com>
2020-05-18 11:16:06 +0100

    Added basic ramps

---
+++ Assets/Tiles/Custom/MirrorRuleTile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class MirrorRuleTile : Tile
+{
+    public RuleTile left, right;
+
+
+    public override void RefreshTile(Vector3Int location, ITilemap tilemap)
+    {
+        base.RefreshTile(location, tilemap);
+
+        TileBase l = tilemap.GetTile(new Vector3Int(location.x-1, location.y, location.z));
+        TileBase r = tilemap.GetTile(new Vector3Int(location.x+1, location.y, location.z));
+
+        // Tiles on both sides
+        if(l != null && r != null)
+        {
+
+        }
+        // Tile only on the left
+        else if(l != null && r == null)
+        {
+
+        }
+        // Tile only on the right 
+        else if (l == null && r != null)
+        {
+
+        }
+        // No tiles left or right
+        else
+        {
+
+        }
+    }
+
+
+}
